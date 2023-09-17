7460c185dc47740e50004f3916a1f2ade453b684
SehyeonKang <80624927+SehyeonKang@users.noreply.github.com>
2023-09-17 17:53:27 +0900

    [D3] Title: 세영이의 SEM력 연도, Time: 1,045 ms, Memory: 23,864 KB -BaekjoonHub

---
+++ SWEA/D3/7532. 세영이의 SEM력 연도/README.md
@@ -0,0 +1,11 @@
+# [D3] 세영이의 SEM력 연도 - 7532 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWooplJ60l8DFARx) 
+
+### 성능 요약
+
+메모리: 23,864 KB, 시간: 1,045 ms, 코드길이: 2,764 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/7532. 세영이의 SEM력 연도/세영이의 SEM력 연도.java
@@ -0,0 +1,69 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String[] args) throws Exception {
+        //System.setIn(new FileInputStream("src/swea/res/input.txt"));
+        Solution S = new Solution();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int answer = 0;
+
+            if (s == 365)
+                s = 0;
+            if (e == 24)
+                e = 0;
+            if (m == 29)
+                m = 0;
+
+            for (int i = 1; i <= 365 * 24 * 29; i++) {
+                if (i % 365 == s && i % 24 == e && i % 29 == m) {
+                    answer = i;
+                    break;
+                }
+            }
+
+            System.out.println("#" + test_case + " " + answer);
+        }
+    }
+}
