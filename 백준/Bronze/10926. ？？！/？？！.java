40a38b4d04b9ecd97554d6866663686217995b9b
SehyeonKang <80624927+SehyeonKang@users.noreply.github.com>
2023-10-01 21:49:09 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String name = input.next();
+		String result = name.concat("??!");
+		
+		System.out.println(result);
+		
+	}
+}
