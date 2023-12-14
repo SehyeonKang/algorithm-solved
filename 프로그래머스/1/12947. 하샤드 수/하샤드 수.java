dd07162f61bda6a46ffd51daddfdce9fc2b8987e
SehyeonKang <80624927+SehyeonKang@users.noreply.github.com>
2023-12-14 15:12:19 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        String s = String.valueOf(x);
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            sum += Character.getNumericValue(s.charAt(i));
+        }
+        if (x % sum != 0)
+            answer = false;
+        return answer;
+    }
+}
