a0fb7df8f5263d1dabffd6c6d16e2c8254efc8cb
SehyeonKang <80624927+SehyeonKang@users.noreply.github.com>
2024-01-13 14:12:29 +0900

    [D3] Title: 부먹왕국의 차원 관문, Time: 255 ms, Memory: 84,900 KB -BaekjoonHub

---
+++ SWEA/D3/7964. 부먹왕국의 차원 관문/부먹왕국의 차원 관문.java
@@ -0,0 +1,69 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int testCase = 1; testCase <= T; testCase++) {
+			int answer = 0;
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int D = Integer.parseInt(st.nextToken());
+			int[] cities = new int[N + 2];
+			cities[0] = 1;
+			cities[N + 1] = 1;
+			
+			st = new StringTokenizer(br.readLine());
+			for (int i = 1; i <= N; i++) {
+				cities[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			int cnt = 0;
+			for (int idx = 0; idx < N + 2; idx++) {
+				if (cities[idx] == 1) {
+					answer += cnt / D;
+					cnt = 0;
+				} else if (cities[idx] == 0) {
+					cnt++;
+				}
+			}
+			
+			sb.append("#" + testCase + " " + answer + "\n"); 
+ 		}
+		
+		System.out.println(sb.toString());
+	}
+}
