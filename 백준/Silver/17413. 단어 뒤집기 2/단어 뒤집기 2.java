806df28e92a184bc7bef37ab259c8c08126bd702
SehyeonKang <80624927+SehyeonKang@users.noreply.github.com>
2024-01-13 23:19:45 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 196 ms, Memory: 17260 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -1,53 +1,32 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine(), "<> ", true);
 		
-		String answer = "";
-		String S = br.readLine();
 		boolean tagFlag = false;
-		String word = "";
-		
-		for (int i = 0; i < S.length(); i++) {
-			char c = S.charAt(i);
-			
-			if (tagFlag) {
-				if (c == '>') {
-					tagFlag = false;
-				}
-				answer += c;
-				continue;
-			}
-			
-			if (c == '<') {
-				if (word.length() > 0) {
-					answer += reverseString(word);
-					word = "";
-				}
+		while (st.hasMoreTokens()) {
+			String s = st.nextToken();
+			if (s.equals("<")) {
 				tagFlag = true;
-				answer += c;
-				continue;
-			}
-			
-			if (c == ' ') {
-				answer += reverseString(word) + c;
-				word = "";
+				sb.append("<");
+			} else if (s.equals(">")) {
+				tagFlag = false;
+				sb.append(">");
 			} else {
-				word += c;
+				if (!tagFlag) {
+					StringBuilder reverseSb = new StringBuilder(s);
+					sb.append(reverseSb.reverse().toString());
+				} else {
+					sb.append(s);
+				}
 			}
 		}
 		
-		if (word.length() > 0) {
-			answer += reverseString(word);
-		}
-		
-		System.out.println(answer);
-	}
-	
-	static String reverseString(String s) {
-		StringBuffer sb = new StringBuffer(s);
-		return sb.reverse().toString();
+		System.out.println(sb);
 	}
 }
