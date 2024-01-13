c43859c7186ffa61d3ce009d1162c45e6b600220
SehyeonKang <80624927+SehyeonKang@users.noreply.github.com>
2024-01-13 15:26:56 +0900

    [D3] Title: 저수지의 물의 총 깊이 구하기, Time: 111 ms, Memory: 19,484 KB -BaekjoonHub

---
+++ SWEA/D3/7236. 저수지의 물의 총 깊이 구하기/README.md
@@ -0,0 +1,15 @@
+# [D3] 저수지의 물의 총 깊이 구하기 - 7236 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWlTKTUqCN8DFAVS) 
+
+### 성능 요약
+
+메모리: 19,484 KB, 시간: 111 ms, 코드길이: 2,731 Bytes
+
+### 제출 일자
+
+2024-01-13 15:26
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/7236. 저수지의 물의 총 깊이 구하기/저수지의 물의 총 깊이 구하기.java
@@ -0,0 +1,80 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int testCase = 1; testCase <= T; testCase++) {
+			int answer = 0;
+			int N = Integer.parseInt(br.readLine());
+			char[][] map = new char[N][N]; 
+			
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					map[i][j] = st.nextToken().charAt(0);
+				}
+			}
+			
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (map[i][j] == 'W') {
+						int depth = 0;
+						
+						for (int row = -1; row <= 1; row++) {
+							for (int col = -1; col <= 1; col++) {
+								if (i + row >= 0 && i + row < N && j + col >= 0 && j + col < N && !(row == 0 && col == 0)) {
+									if (map[i + row][j + col] == 'W') {
+										depth++;
+									}
+								}
+							}
+						}
+						
+						if (depth == 0) {
+							depth++;
+						}
+						answer = Math.max(answer, depth);
+					}
+				}
+			}
+			
+			sb.append("#" + testCase + " " + answer + "\n");
+ 		}
+		
+		System.out.println(sb.toString());
+	}
+}
