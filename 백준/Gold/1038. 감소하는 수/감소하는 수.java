8a8470639c455ef324b281fa3b534cd6ca9d36ca
SehyeonKang <80624927+SehyeonKang@users.noreply.github.com>
2024-02-02 22:58:42 +0900

    [Gold V] Title: 감소하는 수, Time: 168 ms, Memory: 16316 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N;
+	static int nCheck = 0;
+	static String answer = "";
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < 10; i++) {
+			int[] numbers = new int[i + 1];
+			recur(numbers, 0, i + 1);
+		}
+		
+		if (answer.length() == 0) {
+			System.out.println(-1);
+		} else {
+			System.out.println(answer);
+		}
+	}
+	
+	private static void recur(int[] numbers, int count, int length) {
+		if (answer.length() > 0) {
+			return;
+		}
+		
+		if (count == length) {
+			if (nCheck == N) {
+				for (int i = 0; i < numbers.length; i++) {
+					answer += numbers[i];
+				}
+			}
+			nCheck++;
+			return;
+		}
+		
+		if (count == 0) {
+			for (int i = length - 1; i < 10; i++) {
+				numbers[count] = i;
+				recur(numbers, count + 1, length);
+			}
+		} else {
+			for (int i = 0; i < 10; i++) {
+				if (numbers[count - 1] > i) {
+					numbers[count] = i;
+					recur(numbers, count + 1, length);
+				}
+			}
+		}
+	}
+}
