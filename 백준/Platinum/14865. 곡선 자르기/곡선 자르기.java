56e969a20d55eb7a4d822ef63a142f5b0ca99187
SehyeonKang <80624927+SehyeonKang@users.noreply.github.com>
2024-02-26 22:30:29 +0900

    [Platinum V] Title: 곡선 자르기, Time: 1160 ms, Memory: 336468 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/14865. 곡선 자르기/곡선 자르기.java
+++ 백준/Platinum/14865. 곡선 자르기/곡선 자르기.java
@@ -1,23 +1,40 @@
+import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 	
 	static class Element implements Comparable<Element> {
+		int start, end;
+
+		public Element(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}
+
+		@Override
+		public int compareTo(Element o) {
+			return this.start - o.start;
+		}
+	}
+	
+	static class Info implements Comparable<Info> {
 		int x;
 		char c;
 		
-		public Element(int x, char c) {
+		public Info(int x, char c) {
 			this.x = x;
 			this.c = c;
 		}
 
 		@Override
-		public int compareTo(Element o) {
+		public int compareTo(Info o) {
 			return this.x - o.x;
 		}
 	}
@@ -28,77 +45,51 @@ public class Main {
 		StringTokenizer st;
 		
 		int N = Integer.parseInt(br.readLine());
-		PriorityQueue<Element> pq = new PriorityQueue<>();
+		PriorityQueue<Info> pq = new PriorityQueue<>();
+		ArrayList<Point> elements = new ArrayList<>();
+		int startX = Integer.MAX_VALUE;
+		int startY = Integer.MAX_VALUE;
+		int startIdx = -1;
 		
-		st = new StringTokenizer(br.readLine());
-		int x = Integer.parseInt(st.nextToken());
-		int y = Integer.parseInt(st.nextToken());
-		int startX = x;
-		int startY = y;
-		int preX = Integer.MAX_VALUE;
-		int firstDownX = Integer.MAX_VALUE;
-		
-		for (int i = 1; i < N; i++) {
+		// 제일 왼쪽 하단의 시작 정점 찾으면서 정점 리스트 생성
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			int nx = Integer.parseInt(st.nextToken());
-			int ny = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
 			
-			// y축 부호가 바뀔 경우
-			if ((ny > 0 && y < 0) || (ny < 0 && y > 0)) {
-				// 봉우리가 올라갈 경우
-				if (y < 0) {
-					preX = nx;
-				// 봉우리가 내려갈 경우
-				} else {
-					// 처음에 내려갈 때, 마지막에 올라오는 것과 연결해야함
-					if (preX == Integer.MAX_VALUE) {
-						firstDownX = nx;
-					} else {
-						if (preX < nx) {
-							pq.offer(new Element(preX, '('));
-							pq.offer(new Element(nx, ')'));
-						} else {
-							pq.offer(new Element(nx, '('));
-							pq.offer(new Element(preX, ')'));
-						}
-					}
-				}
+			if (y < 0 && startX > x) {
+				startX = x;
+				startY = y;
+				startIdx = i;
+			}
+			
+			elements.add(new Point(x, y));
+		}
+		
+		ArrayList<Element> mountains = new ArrayList<>();
+		
+		int x = startX;
+		int y = startY;
+		for (int i = 0; i < N; i++) {
+			int idx = (startIdx + i) % N;
+			Point p = elements.get(idx);
+			int nx = p.x;
+			int ny = p.y;
+			
+			// 봉우리가 올라가는 경우
+			if (nx == x && y < 0 && ny > 0) {
+				mountains.add(new Element(x, 0));
+			}
+			// 봉우리가 내려가는 경우
+			if (nx == x && y > 0 && ny < 0) {
+				mountains.get(mountains.size() - 1).end = Math.max(x, mountains.get(mountains.size() - 1).start);
+				mountains.get(mountains.size() - 1).start = Math.min(x, mountains.get(mountains.size() - 1).start);
+				pq.offer(new Info(mountains.get(mountains.size() - 1).start, '('));
+				pq.offer(new Info(mountains.get(mountains.size() - 1).end, ')'));
 			}
 			
 			x = nx;
 			y = ny;
-		}
-		
-		// 첫 정점과 마지막 정점으로 봉우리가 만들어질 경우
-		if (startX == x) {
-			// 처음에 y좌표가 양수여서 봉아리가 내려왔을 경우	
-			if (firstDownX != Integer.MAX_VALUE) {
-				if (x < firstDownX) {
-					pq.offer(new Element(x, '('));
-					pq.offer(new Element(firstDownX, ')'));
-				} else {
-					pq.offer(new Element(firstDownX, '('));
-					pq.offer(new Element(x, ')'));
-				}
-			} else {
-				if (preX < x) {
-					pq.offer(new Element(preX, '('));
-					pq.offer(new Element(x, ')'));
-				} else {
-					pq.offer(new Element(x, '('));
-					pq.offer(new Element(preX, ')'));
-				}
-			}
-			
-		// 처음에 y좌표가 양수여서 봉아리가 내려왔을 경우	
-		} else if (firstDownX != Integer.MAX_VALUE) {
-			if (preX < firstDownX) {
-				pq.offer(new Element(preX, '('));
-				pq.offer(new Element(firstDownX, ')'));
-			} else {
-				pq.offer(new Element(firstDownX, '('));
-				pq.offer(new Element(preX, ')'));
-			}
 		}
 		
 		int noIncludedCnt = 0;
@@ -106,9 +97,9 @@ public class Main {
 		ArrayDeque<Character> stack = new ArrayDeque<>();
 		char preChar = ' ';
 		while (!pq.isEmpty()) {
-			Element e = pq.poll();
+			Info i = pq.poll();
 			
-			if (e.c == ')') {
+			if (i.c == ')') {
 				if (preChar == '(') {
 					noIncludingCnt++;
 				}
@@ -118,9 +109,9 @@ public class Main {
 					noIncludedCnt++;
 				}
 			} else {
-				stack.push(e.c);
+				stack.push(i.c);
 			}
-			preChar = e.c;
+			preChar = i.c;
 		}
 		
 		System.out.println(noIncludedCnt + " " + noIncludingCnt);
