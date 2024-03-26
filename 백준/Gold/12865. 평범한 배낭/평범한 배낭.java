39783d050b0c767c2d2d18eeaa8195dd3706ad6c
SehyeonKang <80624927+SehyeonKang@users.noreply.github.com>
2024-03-26 21:43:34 +0900

    [Gold V] Title: 평범한 배낭, Time: 216 ms, Memory: 53800 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,50 +1,48 @@
-
+import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+	
+	static class Item {
+		int w, v;
 
-    static int N, K;
-    static int[] W, V;
-    static Integer[][] dp;
-
-    public static void main(String[] args) throws IOException {
+		public Item(int w, int v) {
+			this.w = w;
+			this.v = v;
+		}
+	}
+	
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
-        W = new int[N];
-        V = new int[N];
-        dp = new Integer[N][K + 1];
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            W[i] = Integer.parseInt(st.nextToken());
-            V[i] = Integer.parseInt(st.nextToken());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[N + 1][K + 1];
+        Item[] items = new Item[N + 1];
+        for (int i = 1; i <= N; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	items[i] = new Item(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
-
-        System.out.println(recur(N - 1, K));
-
-    }
-
-    private static int recur(int n, int k) {
-
-        if (n < 0)
-            return 0;
-
-        if (dp[n][k] == null) {
-            if (k < W[n]) {
-                dp[n][k] = recur(n - 1, k);
-            } else {
-                dp[n][k] = Math.max(recur(n - 1, k), recur(n - 1, k - W[n]) + V[n]);
-            }
+        
+        for (int i = 1; i <= N; i++) {
+        	for (int j = 1; j <= K; j++) {
+        		if (j >= items[i].w) {
+        			dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - items[i].w] + items[i].v);
+        		} else {
+        			dp[i][j] = dp[i - 1][j];
+        		}
+        	}
         }
-
-        return dp[n][k];
+        
+        System.out.println(dp[N][K]);
     }
 }
