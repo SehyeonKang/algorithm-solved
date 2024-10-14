6382cf53a49871aa23b0a946108179205976daea
SehyeonKang <80624927+SehyeonKang@users.noreply.github.com>
2024-10-15 00:16:21 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,59 +1,44 @@
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
-
     static int N, M;
+    static boolean[] visited;
     static int[] arr;
-    static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-
-        st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
+        visited = new boolean[N + 1];
         arr = new int[M];
-        visit = new boolean[N];
 
-        dfs(0);
-
+        recur(0, 1);
         System.out.println(sb);
     }
 
-    private static void dfs(int depth) {
-
-        if (depth == M) {
-            for (int num : arr) {
-                sb.append(num + " ");
+    static void recur(int cnt, int num) {
+        if (cnt == M) {
+            for (int n : arr) {
+                sb.append(n + " ");
             }
             sb.append("\n");
             return;
         }
 
-        for (int i = 0; i < N; i++) {
-            if (!visit[i]) {
-                if (depth == 0) {
-                    arr[depth] = i + 1;
-                    visit[i] = true;
-                    dfs(depth + 1);
-                    visit[i] = false;
-
-                } else if (arr[depth - 1] < i + 1) {
-                    arr[depth] = i + 1;
-                    visit[i] = true;
-                    dfs(depth + 1);
-                    visit[i] = false;
-                }
+        for (int i = num; i <= N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[cnt] = i;
+                recur(cnt + 1, i + 1);
+                visited[i] = false;
             }
         }
     }
+
 }
