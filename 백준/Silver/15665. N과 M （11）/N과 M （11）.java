f3a592d5e922413db30d9edfe74cec3bb05fb219
SehyeonKang <80624927+SehyeonKang@users.noreply.github.com>
2024-10-15 12:45:13 +0900

    [Silver II] Title: N과 M (11), Time: 1212 ms, Memory: 165236 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+	static int N, M;
+	static int[] arr, selected;
+	static Set<String> set = new HashSet<>();
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		selected = new int[M];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+
+		recur(0);
+		System.out.println(sb);
+	}
+
+	static void recur(int cnt) {
+		if (cnt == M) {
+			StringBuilder tmpSb = new StringBuilder();
+			for (int s : selected) {
+				tmpSb.append(arr[s]).append(" ");
+			}
+
+			if (!set.contains(tmpSb.toString())) {
+				set.add(tmpSb.toString());
+				sb.append(tmpSb).append("\n");
+			}
+
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			selected[cnt] = i;
+			recur(cnt + 1);
+		}
+	}
+
+}
