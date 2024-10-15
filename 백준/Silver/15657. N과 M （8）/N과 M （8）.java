7a46065dbf85389ff507a544fa564417ada22552
SehyeonKang <80624927+SehyeonKang@users.noreply.github.com>
2024-10-15 12:28:42 +0900

    [Silver III] Title: N과 M (8), Time: 152 ms, Memory: 18432 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+	static int N, M;
+	static int[] arr, selected;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		selected = new int[M];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+
+		recur(0, 0);
+		System.out.println(sb);
+	}
+
+	static void recur(int cnt, int idx) {
+		if (cnt == M) {
+			for (int s : selected) {
+				sb.append(arr[s]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = idx; i < N; i++) {
+			selected[cnt] = i;
+			recur(cnt + 1, i);
+		}
+	}
+
+}
