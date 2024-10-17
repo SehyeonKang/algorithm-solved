1bd29e0a2031a023940d510c52551237b0127a23
SehyeonKang <80624927+SehyeonKang@users.noreply.github.com>
2024-10-17 23:12:20 +0900

    [Gold III] Title: 나머지 합, Time: 432 ms, Memory: 112928 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -1,29 +1,30 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
-		
-		int[] arr = new int[M];
-		int sum = 0;
-		for (int i = 0; i < N; i++) {
-			sum = (sum + Integer.parseInt(st.nextToken())) % M;
-			arr[sum]++;
-		}
-		
-		long answer = arr[0];
-		for (int i = 0; i < M; i++) {
-			answer += (long) arr[i] * (arr[i] - 1) / 2;
-		}
-		System.out.println(answer);
-	}
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int mod = 0;
+        int[] arr = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            mod = (mod + Integer.parseInt(st.nextToken())) % M;
+            arr[mod]++;
+        }
+
+        long result = arr[0];
+        for (int i = 0; i < M; i++) {
+            result += ((long) arr[i] * (arr[i] - 1)) / 2;
+        }
+        System.out.println(result);
+    }
+
 }
