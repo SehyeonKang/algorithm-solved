143dee7b049d6d3a87b7f177cbb0d6a319b44daf
SehyeonKang <80624927+SehyeonKang@users.noreply.github.com>
2024-10-17 22:43:58 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 716 ms, Memory: 120004 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,49 +1,55 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int[][] map = new int[N + 1][N + 1];
-		int[][] partSumMap = new int[N + 1][N + 1];
-		
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				int partSum = 0;
-				
-				if (j > 1) {
-					partSum = partSumMap[i][j - 1];
-				}
-				
-				for (int k = 1; k <= i; k++) {
-					partSum += map[k][j];
-				}
-				
-				partSumMap[i][j] = partSum;
-			}
-		}
-		
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x1 = Integer.parseInt(st.nextToken());
-			int y1 = Integer.parseInt(st.nextToken());
-			int x2 = Integer.parseInt(st.nextToken());
-			int y2 = Integer.parseInt(st.nextToken());
-			
-			int sum = partSumMap[x2][y2] - partSumMap[x1 - 1][y2] - partSumMap[x2][y1 - 1] + partSumMap[x1 - 1][y1 - 1];
-			sb.append(sum + "\n");
-		}
-		
-		System.out.println(sb.toString());
-	}
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] partSum = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                int num = Integer.parseInt(st.nextToken());
+
+                if (i == 0 && j == 0) {
+                    partSum[i][j] = num;
+                } else if (i == 0) {
+                    partSum[i][j] = partSum[i][j - 1] + num;
+                } else if (j == 0) {
+                    partSum[i][j] = partSum[i - 1][j] + num;
+                } else {
+                    partSum[i][j] = partSum[i - 1][j] + partSum[i][j - 1] - partSum[i - 1][j - 1] + num;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken()) - 1;
+            int y1 = Integer.parseInt(st.nextToken()) - 1;
+            int x2 = Integer.parseInt(st.nextToken()) - 1;
+            int y2 = Integer.parseInt(st.nextToken()) - 1;
+
+            int sum = 0;
+            if (x1 == 0 && y1 == 0) {
+                sum = partSum[x2][y2];
+            } else if (x1 == 0) {
+                sum = partSum[x2][y2] - partSum[x2][y1 - 1];
+            } else if (y1 == 0) {
+                sum = partSum[x2][y2] - partSum[x1 - 1][y2];
+            } else {
+                sum = partSum[x2][y2] - partSum[x2][y1 - 1] - partSum[x1 - 1][y2] + partSum[x1 - 1][y1 - 1];
+            }
+            sb.append(sum).append("\n");
+        }
+        System.out.println(sb);
+    }
+
 }
