a9d60148f8c2a095e9d978f92e1491840b2fc9da
SehyeonKang <80624927+SehyeonKang@users.noreply.github.com>
2024-10-18 21:08:18 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int answer = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            answer += K / arr[i];
+            K %= arr[i];
+        }
+
+        System.out.println(answer);
+    }
+
+}
